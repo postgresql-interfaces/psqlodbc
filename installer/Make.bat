3e7ec5843c7a8bf76391aed4045d91ef710757a9
Dave Page <dpage@pgadmin.org>
2005-12-09 23:19:13 +0000

    Bump version number in a vain attempt to keep up with Luf...

--- installer/Make.bat
+++ installer/Make.bat
@@ -3,7 +3,7 @@
 if NOT "%1"=="" SET VERSION="%1"
 if NOT "%1"=="" GOTO GOT_VERSION
 
-SET VERSION="08.01.0103"
+SET VERSION="08.01.0105"
 echo.
 echo Version not specified - defaulting to %VERSION%
 echo.
