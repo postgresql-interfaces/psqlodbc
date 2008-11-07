2b945ff1edff7fa1095e99b26a63edc1cdc40d79
Hiroshi Saito <hiroshi@winpg.jp>
2008-11-07 13:33:58 +0000

    Prep release Version 08.03.0400.

--- installer/Make.bat
+++ installer/Make.bat
@@ -9,7 +9,7 @@ if NOT "%1"=="" SET VERSION="%1"
 if NOT "%1"=="" GOTO GOT_VERSION
 
 REM The full version number of the build in XXXX.XX.XX format
-SET VERSION="08.03.0300"
+SET VERSION="08.03.0400"
 
 echo.
 echo Version not specified - defaulting to %VERSION%
