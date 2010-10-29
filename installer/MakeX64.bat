fe752c58d11978384f240aa38fe0a90f4924514e
Hiroshi Inoue <inoue@tpf.co.jp>
2010-10-29 12:39:16 +0000

    Move the setting of folder name of libpq or gssapi related dlls from .wxs file to .bat file.

--- installer/MakeX64.bat
+++ installer/MakeX64.bat
@@ -1,28 +1,90 @@
 @echo off
+setlocal
+
+rem
+rem	The 64bit installer
+rem
+set CPUTYPE=x64
+
+rem
+rem	Set yes when clean operations are needed
+rem
+set CLEANUP=no
+rem
+rem	Set yes so as to build dlls before making the installer
+rem
+set DLLBUILD=no
+
+:getparam
+if /i "%1:~0,8%" == "Drivers=" (
+	set DLLBUILD=%1:~8%
+	shift
+	goto getparam
+) else if /i "%1" == "clean" (
+	set CLEANUP=yes
+	shift
+	goto getparam
+)
+
+if "%CLEANUP%" == "yes" GOTO CLEAN
+
+rem
+rem	psqlodbc dlls build options
+rem
+SET USE_LIBPQ=no
+SET USE_GSS=yes
+SET USE_SSPI=yes
+
+rem	
+rem	Please specify the foler name where you placed libpq related dlls.
+rem	Currently not used.
+rem
+rem SET LIBPQBINDIR=
+echo LIBPQBINDIR=%LIBPQBINDIR%
+
+rem	
+rem	Please specify the foler name where you placed GSSAPI related dlls.
+rem
+SET GSSBINDIR="c:/cygwin/develop/bin/AMD64"
+
+rem
+rem	Build binaries if necessary
+rem
+set origdir=%CD%
+if "%DLLBUILD%" == "yes" (
+cd ..
+nmake /f win64.mak USE_LIBPQ=%USE_LIBPQ% USE_SSPI=%USE_SSPI% USE_GSS=%USE_GSS%
+nmake /f win64.mak ANSI_VERSION=yes USE_LIBPQ=%USE_LIBPQ% USE_SSPI=%USE_SSPI% USE_GSS=%USE_GSS%
+)
+cd %origdir%
+
 set wix_dir="%WIX%bin"
 echo wix_dir=%wix_dir%
-REM Values to change include VERSION and SUBLOC, both below.
 
-REM The subdirectory to install into
+rem Values to change include VERSION and SUBLOC, both below.
+
+rem The subdirectory to install into
 SET SUBLOC="0900"
-SET CPUTYPE="x64"
-
-if "%1" == "clean" GOTO CLEAN
 
 if NOT "%1"=="" SET VERSION="%1"
 if NOT "%1"=="" GOTO GOT_VERSION
-GOTO NORMAL_EXEC:
+GOTO NORMAL_EXEC
 
 :CLEAN
 echo.
 echo cleaning derived files
 echo.
 del %CPUTYPE%\psqlodbc*.wix* %CPUTYPE%\psqlodbc*.ms*
+if "%DLLBUILD%" == "yes" (
+cd ..
+nmake /f win64.mak clean
+nmake /f win64.mak ANSI_VERSION=yes clean
+)
 GOTO EXIT
 
 :NORMAL_EXEC
 REM The full version number of the build in XXXX.XX.XX format
-SET VERSION="09.00.0101"
+SET VERSION="09.00.0102"
 
 echo.
 echo Version not specified - defaulting to %VERSION%
@@ -35,7 +97,7 @@ if not exist %CPUTYPE%\ mkdir %CPUTYPE%
 echo.
 echo Building psqlODBC merge module...
 
-%wix_dir%\candle.exe -nologo -dPlatform="%CPUTYPE%" -dVERSION=%VERSION% -dSUBLOC=%SUBLOC% -dSYSTEM32DIR="%SystemRoot%/system32" -o %CPUTYPE%\psqlodbcm.wixobj psqlodbcm_cpu.wxs
+%wix_dir%\candle.exe -nologo -dPlatform="%CPUTYPE%" -dVERSION=%VERSION% -dSUBLOC=%SUBLOC% -dLIBPQBINDIR=%LIBPQBINDIR% -dGSSBINDIR=%GSSBINDIR% -o %CPUTYPE%\psqlodbcm.wixobj psqlodbcm_cpu.wxs
 IF ERRORLEVEL 1 GOTO ERR_HANDLER
 
 echo Linking psqlODBC merge module...
