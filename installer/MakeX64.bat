24dbc591f48a8ef0f9416adc1ad47896f53fc58e
Hiroshi Saito <hiroshi@winpg.jp>
2012-08-16 12:35:06 +0000

    Prep release 09.01.0200

--- installer/MakeX64.bat
+++ installer/MakeX64.bat
@@ -86,7 +86,7 @@ GOTO EXIT
 
 :NORMAL_EXEC
 REM The full version number of the build in XXXX.XX.XX format
-SET VERSION="09.01.0100"
+SET VERSION="09.01.0200"
 
 echo.
 echo Version not specified - defaulting to %VERSION%
