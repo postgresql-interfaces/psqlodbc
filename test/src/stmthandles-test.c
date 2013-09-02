764586c5f1740bbe83d41eb8f12f7641f2409cc9
Heikki Linnakangas <heikki.linnakangas@iki.fi>
2013-09-02 11:29:46 +0300

    Add test case for interleaving operations on several prepared statements.

--- test/src/stmthandles-test.c
+++ test/src/stmthandles-test.c
@@ -15,11 +15,14 @@
  */
 #define NUM_STMT_HANDLES 100
 
+#define MAX_QUERY_SIZE 100
+
 int main(int argc, char **argv)
 {
 	HSTMT hstmt[NUM_STMT_HANDLES];
 	SQLRETURN rc;
 	int i, nhandles;
+	char *sqlbufs[NUM_STMT_HANDLES];
 
 	test_connect();
 
@@ -38,15 +41,16 @@ int main(int argc, char **argv)
 			printf("%d statements allocated...\n", i + 1);
 			fflush(stdout);
 		}
+		/* allocate some space to hold the query */
+		sqlbufs[i] = malloc(MAX_QUERY_SIZE);
 	}
 	nhandles = i;
 
 	/* Execute a query using each of them to verify they all work */
 	for (i = 0; i < nhandles; i++)
 	{
-		char sqlbuf[100];
-		snprintf(sqlbuf, sizeof(sqlbuf), "SELECT 'stmt no %d'", i + 1);
-		rc = SQLExecDirect(hstmt[i], (SQLCHAR *) sqlbuf, SQL_NTS);
+		sprintf(sqlbufs[i], "SELECT 'stmt no %d'", i + 1);
+		rc = SQLExecDirect(hstmt[i], (SQLCHAR *) sqlbufs[i], SQL_NTS);
 		if (!SQL_SUCCEEDED(rc))
 		{
 			print_diag("SQLExecDirect failed", SQL_HANDLE_STMT, hstmt);
@@ -63,6 +67,42 @@ int main(int argc, char **argv)
 	{
 		print_result(hstmt[i]);
 		fflush(stdout);
+	}
+
+	for (i = 0; i < nhandles; i++)
+	{
+		rc = SQLFreeStmt(hstmt[i], SQL_CLOSE);
+		CHECK_STMT_RESULT(rc, "SQLFreeStmt failed", hstmt[i]);
+	}
+
+	/* Try SQLPrepare/SQLExecute on multiple statements. */
+	printf("\nTesting interleaving operations on multiple prepared statements\n");
+	for (i = 0; i < 5; i++)
+	{
+		SQLSMALLINT colcount;
+		int j;
+		char *sqlbuf = sqlbufs[i];
+
+		sprintf(sqlbuf, "SELECT 'stmt no %d'", i);
+		for (j = 0; j < i; j++)
+		{
+			sprintf(&sqlbuf[strlen(sqlbuf)], ", 'col %d'", j);
+		}
+		strcat(sqlbuf, " FROM generate_series(1, 3)");
+		rc = SQLPrepare(hstmt[i], (SQLCHAR *) sqlbuf, SQL_NTS);
+		CHECK_STMT_RESULT(rc, "SQLPrepare failed", hstmt[i]);
+
+		/* Test SQLNumResultCols, called before SQLExecute() */
+		rc = SQLNumResultCols(hstmt[i], &colcount);
+		CHECK_STMT_RESULT(rc, "SQLNumResultCols failed", hstmt);
+		printf("# of result cols: %d\n", colcount);
+	}
+
+	for (i = 0; i < 5; i++)
+	{
+		rc = SQLExecute(hstmt[i]);
+		CHECK_STMT_RESULT(rc, "SQLExecute failed", hstmt[i]);
+		print_result(hstmt[i]);
 	}
 
 	test_disconnect();
