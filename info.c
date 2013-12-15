postgresql-interfaces/psqlodbc

commit d293c1bfbab69660683b07fd1fe3e0cc3cad570e

Replace calls to my_strcat and my_strcat with snprintf_add.
my_strcat didn't check for buffer overflow, which Fortify static analysis
tool flagged as an issue. I'm not sure if that can actually happen, but
the code is clearer like this anyway.

info.c
@@ -1665,8 +1665,10 @@ retry_public_schema:
 			/* strcat(tables_query, " and pg_catalog.pg_table_is_visible(c.oid)"); */
 		}
 		else
-			my_strcat1(tables_query, " and usename %s'%.*s'", op_string, escSchemaName, SQL_NTS);
-		my_strcat1(tables_query, " and relname %s'%.*s'", op_string, escTableName, SQL_NTS);
+			snprintf_add(tables_query, sizeof(tables_query),
+						 " and usename %s'%s'", op_string, escSchemaName);
+		snprintf_add(tables_query, sizeof(tables_query),
+					 " and relname %s'%s'", op_string, escTableName);
 	}
 
 	/* Parse the extra systable prefix	*/
@@ -2143,10 +2145,13 @@ retry_public_schema:
 			"  and a.atttypid = t.oid and (a.attnum > 0)",
 			PG_VERSION_LE(conn, 6.2) ? "a.attlen" : "a.atttypmod");
 		if (escTableName)
-			snprintf_add(columns_query, sizeof(columns_query), " and c.relname %s'%s'", op_string, escTableName);
-		my_strcat1(columns_query, " and u.usename %s'%.*s'", op_string, escSchemaName, SQL_NTS);
+			snprintf_add(columns_query, sizeof(columns_query),
+						 " and c.relname %s'%s'", op_string, escTableName);
+		snprintf_add(columns_query, sizeof(columns_query),
+					 " and u.usename %s'%s'", op_string, escSchemaName);
 		if (escColumnName)
-			snprintf_add(columns_query, sizeof(columns_query), " and a.attname %s'%s'", op_string, escColumnName);
+			snprintf_add(columns_query, sizeof(columns_query),
+						 " and a.attname %s'%s'", op_string, escColumnName);
 		strcat(columns_query, " order by c.relname, attnum");
 	}
 
@@ -2809,12 +2814,14 @@ retry_public_schema:
 	/* TableName cannot contain a string search pattern */
 	/* my_strcat(columns_query, " and c.relname = '%.*s'", szTableName, cbTableName); */
 	if (escTableName)
-		snprintf_add(columns_query, sizeof(columns_query), " and c.relname %s'%s'", eq_string, escTableName);
+		snprintf_add(columns_query, sizeof(columns_query),
+					 " and c.relname %s'%s'", eq_string, escTableName);
 	/* SchemaName cannot contain a string search pattern */
 	if (conn->schema_support)
 		schema_strcat1(columns_query, " and u.nspname %s'%.*s'", eq_string, escSchemaName, SQL_NTS, szTableName, cbTableName, conn);
 	else
-		my_strcat1(columns_query, " and u.usename %s'%.*s'", eq_string, escSchemaName, SQL_NTS);
+		snprintf_add(columns_query, sizeof(columns_query),
+					 " and u.usename %s'%s'", eq_string, escSchemaName);
 
 
 	result = PGAPI_AllocStmt(conn, &hcol_stmt, 0);
@@ -5088,11 +5095,15 @@ PGAPI_ProcedureColumns(
 				   " p.pronamespace = n.oid  and"
 				   " (not proretset) and");
 #endif /* PRORET_COUNT */
-		strcat(proc_query, " has_function_privilege(p.oid, 'EXECUTE')");
-		my_strcat1(proc_query, " and nspname %s'%.*s'", op_string, escSchemaName, SQL_NTS);
+		snprintf_add(proc_query, sizeof(proc_query),
+					 " has_function_privilege(p.oid, 'EXECUTE')"
+					 " and nspname %s'%s'",
+					 op_string, escSchemaName);
 		if (escProcName)
-			snprintf_add(proc_query, sizeof(proc_query), " and proname %s'%s'", op_string, escProcName);
-		strcat(proc_query, " order by nspname, proname, p.oid, attnum");
+			snprintf_add(proc_query, sizeof(proc_query),
+						 " and proname %s'%s'", op_string, escProcName);
+		snprintf_add(proc_query, sizeof(proc_query),
+					 " order by nspname, proname, p.oid, attnum");
 	}
 	else
 	{
@@ -5102,7 +5113,8 @@ PGAPI_ProcedureColumns(
 		ret_col = 5;
 		if (escProcName)
 			snprintf_add(proc_query, sizeof(proc_query), " and proname %s'%s'", op_string, escProcName);
-		strcat(proc_query, " order by proname, proretset");
+		snprintf_add(proc_query, sizeof(proc_query),
+					 " order by proname, proretset");
 	}
 	if (tres = CC_send_query(conn, proc_query, NULL, IGNORE_ABORT_ON_CONN, stmt), !QR_command_maybe_successful(tres))
 	{
@@ -5465,16 +5477,19 @@ PGAPI_Procedures(
 		   " pg_catalog.pg_proc"
 		  " where pg_proc.pronamespace = pg_namespace.oid");
 		schema_strcat1(proc_query, " and nspname %s'%.*s'", op_string, escSchemaName, SQL_NTS, szProcName, cbProcName, conn);
-		my_strcat1(proc_query, " and proname %s'%.*s'", op_string, escProcName, SQL_NTS);
+		snprintf_add(proc_query, sizeof(proc_query),
+					 " and proname %s'%s'", op_string, escProcName);
 	}
 	else
 	{
-		strcpy(proc_query, "select '' as " "PROCEDURE_CAT" ", '' as " "PROCEDURE_SCHEM" ","
-		" proname as " "PROCEDURE_NAME" ", '' as " "NUM_INPUT_PARAMS" ","
-		   " '' as " "NUM_OUTPUT_PARAMS" ", '' as " "NUM_RESULT_SETS" ","
-		   " '' as " "REMARKS" ","
-		   " case when prorettype = 0 then 1::int2 else 2::int2 end as " "PROCEDURE_TYPE" " from pg_proc");
-		my_strcat1(proc_query, " where proname %s'%.*s'", op_string, escSchemaName, SQL_NTS);
+		snprintf(proc_query, sizeof(proc_query),
+				 "select '' as " "PROCEDURE_CAT" ", '' as " "PROCEDURE_SCHEM" ","
+				 " proname as " "PROCEDURE_NAME" ", '' as " "NUM_INPUT_PARAMS" ","
+				 " '' as " "NUM_OUTPUT_PARAMS" ", '' as " "NUM_RESULT_SETS" ","
+				 " '' as " "REMARKS" ","
+				 " case when prorettype = 0 then 1::int2 else 2::int2 end as " "PROCEDURE_TYPE" " from pg_proc"
+				 " where proname %s'%s'",
+				 op_string, escSchemaName);
 	}
 
 	if (res = CC_send_query(conn, proc_query, NULL, IGNORE_ABORT_ON_CONN, stmt), !QR_command_maybe_successful(res))

misc.c
@@ -200,7 +200,7 @@ make_lstring_ifneeded(ConnectionClass *conn, const SQLCHAR *s, ssize_t len, BOOL
  *	This is heavily used in creating queries for info routines (SQLTables, SQLColumns).
  *	This routine could be modified to use vsprintf() to handle multiple arguments.
  */
-char *
+static char *
 my_strcat(char *buf, const char *fmt, const char *s, ssize_t len)
 {
 	if (s && (len > 0 || (len == SQL_NTS && strlen(s) > 0)))
@@ -267,7 +267,7 @@ my_trim(char *s)
  *	my_strcat1 is a extension of my_strcat.
  *	It can have 1 more parameter than my_strcat.
  */
-char *
+static char *
 my_strcat1(char *buf, const char *fmt, const char *s1, const char *s, ssize_t len)
 {
 	ssize_t	length = len;

misc.h
@@ -26,10 +26,8 @@ size_t		strlcat(char *, const char *, size_t);
 char	   *my_trim(char *string);
 char	   *make_string(const char *s, ssize_t len, char *buf, size_t bufsize);
 SQLCHAR	*make_lstring_ifneeded(ConnectionClass *, const SQLCHAR *s, ssize_t len, BOOL);
-char	   *my_strcat(char *buf, const char *fmt, const char *s, ssize_t len);
 char	   *schema_strcat(char *buf, const char *fmt, const char *s, ssize_t len,
 		const char *, int, ConnectionClass *conn);
-char	   *my_strcat1(char *buf, const char *fmt, const char *s1, const char *s, ssize_t len);
 char	   *schema_strcat1(char *buf, const char *fmt, const char *s1,
 				const char *s, ssize_t len,
 				const char *, int, ConnectionClass *conn);

commit e61352e0deb34b197056eb1989538646de3b3644

Fix the bug introduced by   commit d293c1bfbab69660683b07fd1fe3e0cc3cad570e     Replace calls to my_strcat and my_strcat with snprintf_add.
Note that my_strcat1() does nothing when the 4th parameter doesn't
have a valid value.

info.c
@@ -1479,6 +1479,9 @@ CSTR	like_op_sp = 	"like ";
 CSTR	like_op_ext =	"like E";
 CSTR	eq_op_sp =	"= ";
 CSTR	eq_op_ext =	"= E";
+
+#define	 IS_VALID_NAME(str) ((str) && (str)[0])
+
 static const char *gen_opestr(const char *orig_opestr, const ConnectionClass * conn)
 {
 	BOOL	addE = (0 != CC_get_escape(conn) && PG_VERSION_GE(conn, 8.1));
@@ -1665,9 +1668,13 @@ retry_public_schema:
 			/* strcat(tables_query, " and pg_catalog.pg_table_is_visible(c.oid)"); */
 		}
 		else
-			snprintf_add(tables_query, sizeof(tables_query),
+		{
+			if (IS_VALID_NAME(escSchemaName))
+				snprintf_add(tables_query, sizeof(tables_query),
 						 " and usename %s'%s'", op_string, escSchemaName);
-		snprintf_add(tables_query, sizeof(tables_query),
+		}
+		if (IS_VALID_NAME(escTableName))
+			snprintf_add(tables_query, sizeof(tables_query),
 					 " and relname %s'%s'", op_string, escTableName);
 	}
 
@@ -2147,7 +2154,8 @@ retry_public_schema:
 		if (escTableName)
 			snprintf_add(columns_query, sizeof(columns_query),
 						 " and c.relname %s'%s'", op_string, escTableName);
-		snprintf_add(columns_query, sizeof(columns_query),
+		if (IS_VALID_NAME(escSchemaName))
+			snprintf_add(columns_query, sizeof(columns_query),
 					 " and u.usename %s'%s'", op_string, escSchemaName);
 		if (escColumnName)
 			snprintf_add(columns_query, sizeof(columns_query),
@@ -2820,8 +2828,11 @@ retry_public_schema:
 	if (conn->schema_support)
 		schema_strcat1(columns_query, " and u.nspname %s'%.*s'", eq_string, escSchemaName, SQL_NTS, szTableName, cbTableName, conn);
 	else
-		snprintf_add(columns_query, sizeof(columns_query),
+	{
+		if (IS_VALID_NAME(escSchemaName))
+			snprintf_add(columns_query, sizeof(columns_query),
 					 " and u.usename %s'%s'", eq_string, escSchemaName);
+	}
 
 
 	result = PGAPI_AllocStmt(conn, &hcol_stmt, 0);
@@ -5096,7 +5107,9 @@ PGAPI_ProcedureColumns(
 				   " (not proretset) and");
 #endif /* PRORET_COUNT */
 		snprintf_add(proc_query, sizeof(proc_query),
-					 " has_function_privilege(p.oid, 'EXECUTE')"
+					 " has_function_privilege(p.oid, 'EXECUTE')");
+		if (IS_VALID_NAME(escSchemaName))
+			snprintf_add(proc_query, sizeof(proc_query),
 					 " and nspname %s'%s'",
 					 op_string, escSchemaName);
 		if (escProcName)
@@ -5477,7 +5490,8 @@ PGAPI_Procedures(
 		   " pg_catalog.pg_proc"
 		  " where pg_proc.pronamespace = pg_namespace.oid");
 		schema_strcat1(proc_query, " and nspname %s'%.*s'", op_string, escSchemaName, SQL_NTS, szProcName, cbProcName, conn);
-		snprintf_add(proc_query, sizeof(proc_query),
+		if (IS_VALID_NAME(escProcName))
+			snprintf_add(proc_query, sizeof(proc_query),
 					 " and proname %s'%s'", op_string, escProcName);
 	}
 	else
@@ -5487,7 +5501,9 @@ PGAPI_Procedures(
 				 " proname as " "PROCEDURE_NAME" ", '' as " "NUM_INPUT_PARAMS" ","
 				 " '' as " "NUM_OUTPUT_PARAMS" ", '' as " "NUM_RESULT_SETS" ","
 				 " '' as " "REMARKS" ","
-				 " case when prorettype = 0 then 1::int2 else 2::int2 end as " "PROCEDURE_TYPE" " from pg_proc"
+				 " case when prorettype = 0 then 1::int2 else 2::int2 end as " "PROCEDURE_TYPE" " from pg_proc");
+		if (IS_VALID_NAME(escSchemaName))
+			snprintf_add(proc_query, sizeof(proc_query),
 				 " where proname %s'%s'",
 				 op_string, escSchemaName);
 	}