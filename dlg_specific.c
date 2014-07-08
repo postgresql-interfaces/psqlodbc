postgresql-interfaces/psqlodbc

commit 1177281049e8bb3df5992da5dc1ca31ad892082e

Add KeepaliveTime and KeepaliveInterval options to specify time or interval for keepalive.

dlg_specific.c
@@ -193,11 +193,37 @@ abbrev_sslmode(const char *sslmode, char *abbrevmode)
 	return abbrevmode;
 }
 
+static char *makeKeepaliveConnectString(char *target, const ConnInfo *ci, BOOL abbrev)
+{
+	char	*buf = target;
+	*buf = '\0';
+	if (ci->disable_keepalive)
+		return target;
+	if (ci->keepalive_idle < 0 && ci->keepalive_interval < 0)
+		return target;
+	if (ci->keepalive_idle >= 0)
+	{
+		if (abbrev)
+			sprintf(buf, ABBR_KEEPALIVETIME "=%u;", ci->keepalive_idle);
+		else
+			sprintf(buf, INI_KEEPALIVETIME "=%u;", ci->keepalive_idle);
+		buf = strchr(buf, (int) '\0');
+	}
+	if (ci->keepalive_interval >= 0)
+	{
+		if (abbrev)
+			sprintf(buf, ABBR_KEEPALIVEINTERVAL "=%u;", ci->keepalive_interval);
+		else
+			sprintf(buf, INI_KEEPALIVEINTERVAL "=%u;", ci->keepalive_interval);
+	}
+	return target;
+}
+
 void
 makeConnectString(char *connect_string, const ConnInfo *ci, UWORD len)
 {
 	char		got_dsn = (ci->dsn[0] != '\0');
-	char		encoded_item[LARGE_REGISTRY_LEN];
+	char		encoded_item[LARGE_REGISTRY_LEN], keepaliveStr[64];
 	ssize_t		hlen, nlen, olen;
 	/*BOOL		abbrev = (len <= 400);*/
 	BOOL		abbrev = (len < 1024) || 0 < ci->force_abbrev_connstr;
@@ -268,6 +294,7 @@ inolog("hlen=%d", hlen);
 			INI_BYTEAASLONGVARBINARY "=%d;"
 			INI_USESERVERSIDEPREPARE "=%d;"
 			INI_LOWERCASEIDENTIFIER "=%d;"
+			"%s"
 #ifdef	WIN32
 			INI_GSSAUTHUSEGSSAPI "=%d;"
 #endif /* WIN32 */
@@ -306,6 +333,7 @@ inolog("hlen=%d", hlen);
 			,ci->bytea_as_longvarbinary
 			,ci->use_server_side_prepare
 			,ci->lower_case_identifier
+			,makeKeepaliveConnectString(keepaliveStr, ci, FALSE)
 #ifdef	WIN32
 			,ci->gssauth_use_gssapi
 #endif /* WIN32 */
@@ -399,7 +427,8 @@ inolog("hlen=%d", hlen);
 				ABBR_MAXLONGVARCHARSIZE "=%d;"
 				INI_INT8AS "=%d;"
 				ABBR_EXTRASYSTABLEPREFIXES "=%s;"
-				INI_ABBREVIATE "=%02x%x",
+				ABBR_EXTRASYSTABLEPREFIXES "=%s;"
+				"%s",
 				encoded_item,
 				ci->drivers.fetch_max,
 				ci->drivers.socket_buffersize,
@@ -407,6 +436,7 @@ inolog("hlen=%d", hlen);
 				ci->drivers.max_longvarchar_size,
 				ci->int8_as,
 				ci->drivers.extra_systable_prefixes,
+				makeKeepaliveConnectString(keepaliveStr, ci, TRUE),
 				EFFECTIVE_BIT_COUNT, flag);
 		if (olen < nlen && (PROTOCOL_74(ci) || ci->rollback_on_error >= 0))
 		{
@@ -602,6 +632,10 @@ copyAttributes(ConnInfo *ci, const char *attribute, const char *value)
 		ci->lower_case_identifier = atoi(value);
 	else if (stricmp(attribute, INI_GSSAUTHUSEGSSAPI) == 0 || stricmp(attribute, ABBR_GSSAUTHUSEGSSAPI) == 0)
 		ci->gssauth_use_gssapi = atoi(value);
+	else if (stricmp(attribute, INI_KEEPALIVETIME) == 0 || stricmp(attribute, ABBR_KEEPALIVETIME) == 0)
+		ci->keepalive_idle = atoi(value);
+	else if (stricmp(attribute, INI_KEEPALIVEINTERVAL) == 0 || stricmp(attribute, ABBR_KEEPALIVEINTERVAL) == 0)
+		ci->keepalive_interval = atoi(value);
 	else if (stricmp(attribute, INI_SSLMODE) == 0 || stricmp(attribute, ABBR_SSLMODE) == 0)
 	{
 		switch (value[0])
@@ -983,6 +1017,21 @@ getDSNinfo(ConnInfo *ci, char overwrite)
 			ci->gssauth_use_gssapi = atoi(temp);
 	}
 
+	if (ci->keepalive_idle < 0 || overwrite)
+	{
+		SQLGetPrivateProfileString(DSN, INI_KEEPALIVETIME, "", temp, sizeof(temp), ODBC_INI);
+		if (temp[0])
+			if (0 == (ci->keepalive_idle = atoi(temp)))
+				ci->keepalive_idle = -1;
+	}
+	if (ci->keepalive_interval < 0 || overwrite)
+	{
+		SQLGetPrivateProfileString(DSN, INI_KEEPALIVEINTERVAL, "", temp, sizeof(temp), ODBC_INI);
+		if (temp[0])
+			if (0 == (ci->keepalive_interval = atoi(temp)))
+				ci->keepalive_interval = -1;
+	}
+
 	if (ci->sslmode[0] == '\0' || overwrite)
 		SQLGetPrivateProfileString(DSN, INI_SSLMODE, "", ci->sslmode, sizeof(ci->sslmode), ODBC_INI);
 
@@ -1273,6 +1322,16 @@ writeDSNinfo(const ConnInfo *ci)
 	SQLWritePrivateProfileString(DSN,
 								 INI_SSLMODE,
 								 ci->sslmode,
+								 ODBC_INI);
+	sprintf(temp, "%d", ci->keepalive_idle);
+	SQLWritePrivateProfileString(DSN,
+								 INI_KEEPALIVETIME,
+								 temp,
+								 ODBC_INI);
+	sprintf(temp, "%d", ci->keepalive_interval);
+	SQLWritePrivateProfileString(DSN,
+								 INI_KEEPALIVEINTERVAL,
+								 temp,
 								 ODBC_INI);
 #ifdef	_HANDLE_ENLIST_IN_DTC_
 	sprintf(temp, "%d", ci->xa_opt);

connection.c
@@ -298,6 +298,8 @@ CC_conninfo_init(ConnInfo *conninfo, UInt4 option)
 	conninfo->ignore_round_trip_time = -1;
 	conninfo->disable_keepalive = -1;
 	conninfo->gssauth_use_gssapi = -1;
+	conninfo->keepalive_idle = -1;
+	conninfo->keepalive_interval = -1;
 #ifdef	_HANDLE_ENLIST_IN_DTC_
 	conninfo->xa_opt = -1;
 #endif /* _HANDLE_ENLIST_IN_DTC_ */
@@ -352,6 +354,8 @@ CC_copy_conninfo(ConnInfo *ci, const ConnInfo *sci)
 	CORR_VALCPY(disable_keepalive);
 	CORR_VALCPY(gssauth_use_gssapi);
 	CORR_VALCPY(extra_opts);
+	CORR_VALCPY(keepalive_idle);
+	CORR_VALCPY(keepalive_interval);
 #ifdef	_HANDLE_ENLIST_IN_DTC_
 	CORR_VALCPY(xa_opt);
 #endif
@@ -1336,6 +1340,9 @@ static int	protocol3_packet_build(ConnectionClass *self)
 
 #ifdef USE_LIBPQ
 CSTR	l_login_timeout = "connect_timeout";
+CSTR	l_keepalives_idle = "keepalives_idle";
+CSTR	l_keepalives_interval = "keepalives_interval";
+
 static char	*protocol3_opts_build(ConnectionClass *self)
 {
 	CSTR	func = "protocol3_opts_build";
@@ -1359,10 +1366,27 @@ static char	*protocol3_opts_build(ConnectionClass *self)
 	{
 		char	tmout[16];
 
-		slen += (strlen(l_login_timeout) + 2 + 2);
+		slen += (strlen(l_login_timeout) + 2);
 		snprintf(tmout, sizeof(tmout), FORMAT_UINTEGER, self->login_timeout);
 		slen += strlen(tmout);
 	}
+	if (self->connInfo.keepalive_idle > 0)
+	{
+		char	outwrk[16];
+
+		slen += (strlen(l_keepalives_idle) + 2);
+		snprintf(outwrk, sizeof(outwrk), FORMAT_UINTEGER, self->connInfo.keepalive_idle);
+		slen += strlen(outwrk);
+	}
+	if (self->connInfo.keepalive_interval > 0)
+	{
+		char	outwrk[16];
+
+		slen += (strlen(l_keepalives_interval) + 2);
+		snprintf(outwrk, sizeof(outwrk), FORMAT_UINTEGER, self->connInfo.keepalive_interval);
+		slen += strlen(outwrk);
+	}
+
 	slen++;
 
 	if (conninfo = malloc(slen), !conninfo)
@@ -1396,9 +1420,23 @@ static char	*protocol3_opts_build(ConnectionClass *self)
 	if (self->login_timeout > 0)
 	{
 		sprintf(ppacket, " %s=", l_login_timeout);
-		ppacket += (strlen(l_login_timeout) + 2);
+		ppacket = strchr(ppacket, (int) '\0');
 		sprintf(ppacket, FORMAT_UINTEGER, self->login_timeout);
-		ppacket = strchr(ppacket, '\0');
+		ppacket = strchr(ppacket, (int) '\0');
+	}
+	if (self->connInfo.keepalive_idle > 0)
+	{
+		sprintf(ppacket, " %s=", l_keepalives_idle);
+		ppacket = strchr(ppacket, (int) '\0');
+		sprintf(ppacket, FORMAT_UINTEGER, self->connInfo.keepalive_idle);
+		ppacket = strchr(ppacket, (int) '\0');
+	}
+	if (self->connInfo.keepalive_interval > 0)
+	{
+		sprintf(ppacket, " %s=", l_keepalives_interval);
+		ppacket = strchr(ppacket, (int) '\0');
+		sprintf(ppacket, FORMAT_UINTEGER, self->connInfo.keepalive_interval);
+		ppacket = strchr(ppacket, (int) '\0');
 	}
 	*ppacket = '\0';
 inolog("return conninfo=%s(%d)\n", conninfo, strlen(conninfo));

connection.h
@@ -319,6 +319,8 @@ typedef struct
 	signed char	disable_keepalive;
 	signed char	gssauth_use_gssapi;
 	UInt4		extra_opts;
+	Int4		keepalive_idle;
+	Int4		keepalive_interval;
 #ifdef	_HANDLE_ENLIST_IN_DTC_
 	signed char	xa_opt;
 #endif /* _HANDLE_ENLIST_IN_DTC_ */

dlg_specific.h
@@ -147,6 +147,11 @@ extern "C" {
 #define INI_LOGDIR			"Logdir"
 #define INI_GSSAUTHUSEGSSAPI		"GssAuthUseGSS"
 #define ABBR_GSSAUTHUSEGSSAPI		"D0"
+#define INI_KEEPALIVETIME		"KeepaliveTime"
+#define ABBR_KEEPALIVETIME		"D1"
+#define INI_KEEPALIVEINTERVAL		"KeepaliveInterval"
+#define ABBR_KEEPALIVEINTERVAL		"D2"
+#define INI_DTCLOG			"Dtclog"
 
 #define	SSLMODE_DISABLE		"disable"
 #define	SSLMODE_ALLOW		"allow"

dlg_wingui.c
@@ -34,6 +34,8 @@ extern HINSTANCE NEAR s_hModule;
 static int	driver_optionsDraw(HWND, const ConnInfo *, int src, BOOL enable);
 static int	driver_options_update(HWND hdlg, ConnInfo *ci, const char *);
 
+static int	ds_options_update(HWND hdlg, ConnInfo *ci);
+
 static struct {
 	int	ids;
 	const char * const	modestr;
@@ -447,7 +449,7 @@ ds_options1Proc(HWND hdlg,
 						fbuf,
 						sizeof(fbuf));
 				if (cmd <= 0)
-					strcpy(fbuf, "Advanced Options (%s) 1/2");
+					strcpy(fbuf, "Advanced Options (%s) 1/3");
 				sprintf(strbuf, fbuf, ci->dsn);
 				SetWindowText(hdlg, strbuf);
 			}
@@ -494,6 +496,101 @@ ds_options1Proc(HWND hdlg,
 	return FALSE;
 }
 
+static int
+ds_options_update(HWND hdlg, ConnInfo *ci)
+{
+	char		buf[128];
+
+	mylog("%s: got ci = %p\n", __FUNCTION__, ci);
+
+	/* Readonly */
+	sprintf(ci->onlyread, "%d", IsDlgButtonChecked(hdlg, DS_READONLY));
+
+	/* Protocol */
+	if (IsDlgButtonChecked(hdlg, DS_PG62))
+		strcpy(ci->protocol, PG62);
+	else if (IsDlgButtonChecked(hdlg, DS_PG63))
+		strcpy(ci->protocol, PG63);
+	else if (IsDlgButtonChecked(hdlg, DS_PG64))
+		strcpy(ci->protocol, PG64);
+	else
+		/* latest */
+		strcpy(ci->protocol, PG74);
+
+	/* Issue rollback command on error */
+	if (IsDlgButtonChecked(hdlg, DS_NO_ROLLBACK))
+		ci->rollback_on_error = 0;
+	else if (IsDlgButtonChecked(hdlg, DS_TRANSACTION_ROLLBACK))
+		ci->rollback_on_error = 1;
+	else if (IsDlgButtonChecked(hdlg, DS_STATEMENT_ROLLBACK))
+		ci->rollback_on_error = 2;
+	else
+		/* legacy */
+		ci->rollback_on_error = 1;
+
+	/* Int8 As */
+	if (IsDlgButtonChecked(hdlg, DS_INT8_AS_DEFAULT))
+		ci->int8_as = 0;
+	else if (IsDlgButtonChecked(hdlg, DS_INT8_AS_BIGINT))
+		ci->int8_as = SQL_BIGINT;
+	else if (IsDlgButtonChecked(hdlg, DS_INT8_AS_NUMERIC))
+		ci->int8_as = SQL_NUMERIC;
+	else if (IsDlgButtonChecked(hdlg, DS_INT8_AS_DOUBLE))
+		ci->int8_as = SQL_DOUBLE;
+	else if (IsDlgButtonChecked(hdlg, DS_INT8_AS_INT4))
+		ci->int8_as = SQL_INTEGER;
+	else
+		ci->int8_as = SQL_VARCHAR;
+
+	GetDlgItemText(hdlg, DS_EXTRA_OPTIONS, buf, sizeof(buf));
+	setExtraOptions(ci, buf, NULL);
+	sprintf(ci->show_system_tables, "%d", IsDlgButtonChecked(hdlg, DS_SHOWSYSTEMTABLES));
+
+	sprintf(ci->row_versioning, "%d", IsDlgButtonChecked(hdlg, DS_ROWVERSIONING));
+	ci->disallow_premature = IsDlgButtonChecked(hdlg, DS_DISALLOWPREMATURE);
+	ci->lf_conversion = IsDlgButtonChecked(hdlg, DS_LFCONVERSION);
+	ci->true_is_minus1 = IsDlgButtonChecked(hdlg, DS_TRUEISMINUS1);
+	ci->allow_keyset = IsDlgButtonChecked(hdlg, DS_UPDATABLECURSORS);
+	ci->use_server_side_prepare = IsDlgButtonChecked(hdlg, DS_SERVERSIDEPREPARE);
+	ci->bytea_as_longvarbinary = IsDlgButtonChecked(hdlg, DS_BYTEAASLONGVARBINARY);
+	/*ci->lower_case_identifier = IsDlgButtonChecked(hdlg, DS_LOWERCASEIDENTIFIER);*/
+	ci->gssauth_use_gssapi = IsDlgButtonChecked(hdlg, DS_GSSAUTHUSEGSSAPI);
+
+	/* OID Options */
+	sprintf(ci->fake_oid_index, "%d", IsDlgButtonChecked(hdlg, DS_FAKEOIDINDEX));
+	sprintf(ci->show_oid_column, "%d", IsDlgButtonChecked(hdlg, DS_SHOWOIDCOLUMN));
+
+	/* Datasource Connection Settings */
+	{
+		char conn_settings[LARGE_REGISTRY_LEN];
+		GetDlgItemText(hdlg, DS_CONNSETTINGS, conn_settings, sizeof(conn_settings));
+		if ('\0' != conn_settings[0])
+			STR_TO_NAME(ci->conn_settings, conn_settings);
+	}
+
+	/* TCP KEEPALIVE */
+	ci->disable_keepalive = IsDlgButtonChecked(hdlg, DS_DISABLE_KEEPALIVE);
+	if (ci->disable_keepalive)
+	{
+		ci->keepalive_idle = -1;
+		ci->keepalive_interval = -1;
+	}
+	else
+	{
+		char	temp[64];
+		int	val;
+
+		GetDlgItemText(hdlg, DS_KEEPALIVETIME, temp, sizeof(temp));
+		if  (val = atoi(temp), 0 == val)
+			val = -1;
+		ci->keepalive_idle = val;
+		GetDlgItemText(hdlg, DS_KEEPALIVEINTERVAL, temp, sizeof(temp));
+		if  (val = atoi(temp), 0 == val)
+			val = -1;
+		ci->keepalive_interval = val;
+	}
+	return 0;
+}
 
 LRESULT			CALLBACK
 ds_options2Proc(HWND hdlg,
@@ -522,7 +619,7 @@ ds_options2Proc(HWND hdlg,
 						fbuf,
 						sizeof(fbuf));
 				if (cmd <= 0)
-					strcpy(fbuf, "Advanced Options (%s) 2/2");
+					strcpy(fbuf, "Advanced Options (%s) 2/3");
 				sprintf(buf, fbuf, ci->dsn);
 				SetWindowText(hdlg, buf);
 			}
@@ -609,108 +706,60 @@ ds_options2Proc(HWND hdlg,
 
 			/* Datasource Connection Settings */
 			SetDlgItemText(hdlg, DS_CONNSETTINGS, SAFE_NAME(ci->conn_settings));
+			/* KEEPALIVE */
+			enable = (0 == (ci->extra_opts & BIT_DISABLE_KEEPALIVE));
+			CheckDlgButton(hdlg, DS_DISABLE_KEEPALIVE, !enable);
+			if (enable)
+			{
+				if (ci->keepalive_idle > 0)
+				{
+					snprintf(buf, sizeof(buf), "%d", ci->keepalive_idle);
+					SetDlgItemText(hdlg, DS_KEEPALIVETIME, buf);
+				}
+				if (ci->keepalive_interval > 0)
+				{
+					snprintf(buf, sizeof(buf), "%d", ci->keepalive_interval);
+					SetDlgItemText(hdlg, DS_KEEPALIVEINTERVAL, buf);
+				}
+			}
 			break;
 
 		case WM_COMMAND:
+			ci = (ConnInfo *) GetWindowLongPtr(hdlg, DWLP_USER);
 			switch (cmd = GET_WM_COMMAND_ID(wParam, lParam))
 			{
 				case DS_SHOWOIDCOLUMN:
 					mylog("WM_COMMAND: DS_SHOWOIDCOLUMN\n");
 					EnableWindow(GetDlgItem(hdlg, DS_FAKEOIDINDEX), IsDlgButtonChecked(hdlg, DS_SHOWOIDCOLUMN));
 					return TRUE;
+				case DS_DISABLE_KEEPALIVE:
+					mylog("WM_COMMAND: DS_SHOWOIDCOLUMN\n");
+					EnableWindow(GetDlgItem(hdlg, DS_KEEPALIVETIME), !IsDlgButtonChecked(hdlg, cmd));
+					EnableWindow(GetDlgItem(hdlg, DS_KEEPALIVEINTERVAL), !IsDlgButtonChecked(hdlg, cmd));
+					return TRUE;
 
 				case IDOK:
-				case IDAPPLY:
-				case IDPREVPAGE:
-					ci = (ConnInfo *) GetWindowLongPtr(hdlg, DWLP_USER);
-					mylog("IDOK: got ci = %p\n", ci);
-
-					/* Readonly */
-					sprintf(ci->onlyread, "%d", IsDlgButtonChecked(hdlg, DS_READONLY));
-
-					/* Protocol */
-					if (IsDlgButtonChecked(hdlg, DS_PG62))
-						strcpy(ci->protocol, PG62);
-					else if (IsDlgButtonChecked(hdlg, DS_PG63))
-						strcpy(ci->protocol, PG63);
-					else if (IsDlgButtonChecked(hdlg, DS_PG64))
-						strcpy(ci->protocol, PG64);
-					else
-						/* latest */
-						strcpy(ci->protocol, PG74);
-
-					/* Issue rollback command on error */
-					if (IsDlgButtonChecked(hdlg, DS_NO_ROLLBACK))
-						ci->rollback_on_error = 0;
-					else if (IsDlgButtonChecked(hdlg, DS_TRANSACTION_ROLLBACK))
-						ci->rollback_on_error = 1;
-					else if (IsDlgButtonChecked(hdlg, DS_STATEMENT_ROLLBACK))
-						ci->rollback_on_error = 2;
-					else
-						/* legacy */
-						ci->rollback_on_error = 1;
-
-					/* Int8 As */
-					if (IsDlgButtonChecked(hdlg, DS_INT8_AS_DEFAULT))
-						ci->int8_as = 0;
-					else if (IsDlgButtonChecked(hdlg, DS_INT8_AS_BIGINT))
-						ci->int8_as = SQL_BIGINT;
-					else if (IsDlgButtonChecked(hdlg, DS_INT8_AS_NUMERIC))
-						ci->int8_as = SQL_NUMERIC;
-					else if (IsDlgButtonChecked(hdlg, DS_INT8_AS_DOUBLE))
-						ci->int8_as = SQL_DOUBLE;
-					else if (IsDlgButtonChecked(hdlg, DS_INT8_AS_INT4))
-						ci->int8_as = SQL_INTEGER;
-					else
-						ci->int8_as = SQL_VARCHAR;
-
-					GetDlgItemText(hdlg, DS_EXTRA_OPTIONS, buf, sizeof(buf));
-					setExtraOptions(ci, buf, NULL);
-					sprintf(ci->show_system_tables, "%d", IsDlgButtonChecked(hdlg, DS_SHOWSYSTEMTABLES));
-
-					sprintf(ci->row_versioning, "%d", IsDlgButtonChecked(hdlg, DS_ROWVERSIONING));
-					ci->disallow_premature = IsDlgButtonChecked(hdlg, DS_DISALLOWPREMATURE);
-					ci->lf_conversion = IsDlgButtonChecked(hdlg, DS_LFCONVERSION);
-					ci->true_is_minus1 = IsDlgButtonChecked(hdlg, DS_TRUEISMINUS1);
-					ci->allow_keyset = IsDlgButtonChecked(hdlg, DS_UPDATABLECURSORS);
-					ci->use_server_side_prepare = IsDlgButtonChecked(hdlg, DS_SERVERSIDEPREPARE);
-					ci->bytea_as_longvarbinary = IsDlgButtonChecked(hdlg, DS_BYTEAASLONGVARBINARY);
-					/*ci->lower_case_identifier = IsDlgButtonChecked(hdlg, DS_LOWERCASEIDENTIFIER);*/
-					ci->gssauth_use_gssapi = IsDlgButtonChecked(hdlg, DS_GSSAUTHUSEGSSAPI);
-
-					/* OID Options */
-					sprintf(ci->fake_oid_index, "%d", IsDlgButtonChecked(hdlg, DS_FAKEOIDINDEX));
-					sprintf(ci->show_oid_column, "%d", IsDlgButtonChecked(hdlg, DS_SHOWOIDCOLUMN));
-
-					/* Datasource Connection Settings */
-					{
-						char conn_settings[LARGE_REGISTRY_LEN];
-						GetDlgItemText(hdlg, DS_CONNSETTINGS, conn_settings, sizeof(conn_settings));
-						if ('\0' != conn_settings[0])
-							STR_TO_NAME(ci->conn_settings, conn_settings);
-					}
-					if (IDAPPLY == cmd)
-					{
-						SendMessage(GetWindow(hdlg, GW_OWNER), WM_COMMAND, wParam, lParam);
-						break;
-					}
-
-					EndDialog(hdlg, cmd == IDOK);
-					if (IDOK == cmd)
-						return TRUE;
-					DialogBoxParam(s_hModule,
-								   MAKEINTRESOURCE(DLG_OPTIONS_DRV),
-								   hdlg, ds_options1Proc, (LPARAM) ci);
-					break;
-
+					ds_options_update(hdlg, ci);
 				case IDCANCEL:
-					EndDialog(hdlg, GET_WM_COMMAND_ID(wParam, lParam) == IDOK);
+					EndDialog(hdlg, IDOK == cmd);
 					return TRUE;
+				case IDAPPLY:
+					ds_options_update(hdlg, ci);
+					SendMessage(GetWindow(hdlg, GW_OWNER), WM_COMMAND, wParam, lParam);
+					break;
+				case IDPREVPAGE:
+					ds_options_update(hdlg, ci);
+					EndDialog(hdlg, cmd == IDOK);
+					DialogBoxParam(s_hModule,
+						MAKEINTRESOURCE(DLG_OPTIONS_DRV),
+						   hdlg, ds_options1Proc, (LPARAM) ci);
+					break;
 			}
 	}
 
 	return FALSE;
 }
+
 
 typedef	SQLRETURN (SQL_API *SQLAPIPROC)();
 static int

psqlodbc.rc
@@ -77,6 +77,11 @@ BEGIN
     PUSHBUTTON      "テスト",IDC_TEST,296,30,51,14
 END
 
+#define	MISC_X
+#define	MISC_Y	155
+#define	MISC_Y1	(MISC_Y+11)
+#define	MISC_Y2	(MISC_Y+33)
+
 DLG_OPTIONS_DRV DIALOG DISCARDABLE  0, 0, 350, 241
 STYLE DS_MODALFRAME | DS_3DLOOK | DS_CENTER | WS_POPUP | WS_CAPTION | 
     WS_SYSMENU
@@ -116,28 +121,33 @@ BEGIN
                     "Button",BS_AUTOCHECKBOX | WS_TABSTOP,123,132,117,10
     CONTROL         "CharとしてBoolsを扱う",DRV_BOOLS_CHAR,"Button",
                     BS_AUTOCHECKBOX | WS_TABSTOP,243,132,96,10
-    LTEXT           "ｷｬｯｼｭｻｲｽﾞ:",IDC_STATIC,21,189,39,8
-    EDITTEXT        DRV_CACHE_SIZE,69,187,35,12,ES_AUTOHSCROLL
-    LTEXT           "最大&Varchar:",IDC_STATIC,13,167,54,8
-    EDITTEXT        DRV_VARCHAR_SIZE,70,166,35,12,ES_AUTOHSCROLL
-    LTEXT           "最大Lon&gVarChar:",IDC_STATIC,148,168,66,8
-    EDITTEXT        DRV_LONGVARCHAR_SIZE,219,166,35,12,ES_AUTOHSCROLL
-    LTEXT           "ｼｽﾃﾑﾃｰﾌﾞﾙ ﾌﾟﾚﾌｨｸｽ:",IDC_STATIC,140,189,74,9
-    EDITTEXT        DRV_EXTRASYSTABLEPREFIXES,219,187,71,12,ES_AUTOHSCROLL
+    GROUPBOX        "その他",IDC_STATIC,5,MISC_Y,340,54
+    LTEXT           "最大&Varchar:",IDC_STATIC,13,MISC_Y1+1,54,8
+    EDITTEXT        DRV_VARCHAR_SIZE,70,MISC_Y1,35,12,ES_AUTOHSCROLL
+    LTEXT           "最大Lon&gVarChar:",IDC_STATIC,148,MISC_Y1+2,66,8
+    EDITTEXT        DRV_LONGVARCHAR_SIZE,219,MISC_Y1,35,12,ES_AUTOHSCROLL
+    LTEXT           "ｷｬｯｼｭｻｲｽﾞ:",IDC_STATIC,21,MISC_Y2+1,39,8
+    EDITTEXT        DRV_CACHE_SIZE,69,MISC_Y2-1,35,12,ES_AUTOHSCROLL
+    LTEXT           "ｼｽﾃﾑﾃｰﾌﾞﾙ ﾌﾟﾚﾌｨｸｽ:",IDC_STATIC,140,MISC_Y2+1,74,9
+    EDITTEXT        DRV_EXTRASYSTABLEPREFIXES,219,MISC_Y2-1,71,12,ES_AUTOHSCROLL
     DEFPUSHBUTTON   "OK",IDOK,5,221,50,15,WS_GROUP
     PUSHBUTTON      "キャンセル",IDCANCEL,68,221,50,15
     PUSHBUTTON      "適用",IDAPPLY,132,221,50,15
     PUSHBUTTON      "デフォルト",IDDEFAULTS,295,221,50,15
-    GROUPBOX        "その他",IDC_STATIC,5,155,340,54
 END
 
-DLG_OPTIONS_DS DIALOG DISCARDABLE  0, 0, 306, 243
+#define	CONNSETTINGS_Y	185	// 191
+#define	KEEPALIVE_X	5
+#define	KEEPALIVE_Y	225
+#define	ENDLINE_Y	245	// 224
+
+DLG_OPTIONS_DS DIALOG DISCARDABLE  0, 0, 306, 260
 STYLE DS_MODALFRAME | DS_3DLOOK | DS_CENTER | WS_POPUP | WS_CAPTION | 
     WS_SYSMENU
 CAPTION "高度な設定 (データソース２)"
 FONT 9, "ＭＳ ゴシック"
 BEGIN
-    PUSHBUTTON      "設定2",IDNEXTPAGE,49,5,40,15
+    PUSHBUTTON      "設定3",IDNEXTPAGE,49,5,40,15
     PUSHBUTTON      "設定1",IDPREVPAGE,5,5,40,15
     CONTROL         "リ－ドオンリィ(&R)",DS_READONLY,"Button",
                     BS_AUTOCHECKBOX | WS_GROUP | WS_TABSTOP,15,26,102,10
@@ -192,19 +202,26 @@ BEGIN
                     BS_AUTOCHECKBOX | WS_GROUP,16,169,72,10
     CONTROL         "インデックスを装う(&I)",DS_FAKEOIDINDEX,"Button",
                     BS_AUTOCHECKBOX | WS_TABSTOP,107,169,95,10
-    LTEXT           "接続時   設定:(&S)",IDC_STATIC,13,192,69,10,NOT 
+    LTEXT           "接続時   設定:(&S)",IDC_STATIC,13,CONNSETTINGS_Y+3,69,10,NOT 
                     WS_GROUP
-    EDITTEXT        DS_CONNSETTINGS,90,191,211,27,ES_MULTILINE | 
+    EDITTEXT        DS_CONNSETTINGS,90,CONNSETTINGS_Y,211,27,ES_MULTILINE | 
                     ES_AUTOVSCROLL | ES_AUTOHSCROLL | ES_WANTRETURN | NOT 
                     WS_TABSTOP
-    DEFPUSHBUTTON   "OK",IDOK,5,224,50,14,WS_GROUP
-    PUSHBUTTON      "キャンセル",IDCANCEL,66,224,50,14
-    PUSHBUTTON      "適用",IDAPPLY,128,224,50,14
+    GROUPBOX        "TCP KEEPALIVE(秒単位)",IDC_STATIC,KEEPALIVE_X,KEEPALIVE_Y-10,210,25
+    CONTROL         "利用しない",DS_DISABLE_KEEPALIVE,"Button",BS_AUTOCHECKBOX | WS_GROUP, KEEPALIVE_X+5,KEEPALIVE_Y,50,10
+    LTEXT           "待ち時間",IDC_STATIC,KEEPALIVE_X+62,KEEPALIVE_Y,35,10,NOT WS_GROUP
+    EDITTEXT        DS_KEEPALIVETIME,KEEPALIVE_X+100,KEEPALIVE_Y,30,10, ES_AUTOHSCROLL | WS_TABSTOP
+    LTEXT           "応答待時間",IDC_STATIC,KEEPALIVE_X+137,KEEPALIVE_Y,40,10,NOT WS_GROUP
+    EDITTEXT        DS_KEEPALIVEINTERVAL,KEEPALIVE_X+180,KEEPALIVE_Y,30,10, ES_AUTOHSCROLL | WS_TABSTOP
+    DEFPUSHBUTTON   "OK",IDOK,5,ENDLINE_Y,50,14,WS_GROUP
+    PUSHBUTTON      "キャンセル",IDCANCEL,66,ENDLINE_Y,50,14
+    PUSHBUTTON      "適用",IDAPPLY,128,ENDLINE_Y,50,14
     CONTROL         "byteaをLOとして扱う",DS_BYTEAASLONGVARBINARY,"Button",
                     BS_AUTOCHECKBOX | WS_TABSTOP,15,85,87,10
     CONTROL         "Kerberos応答にGSSAPI利用",DS_GSSAUTHUSEGSSAPI,"Button",
                     BS_AUTOCHECKBOX | WS_TABSTOP,139,85,110,10
 END
+
 
 DLG_OPTIONS_GLOBAL DIALOG DISCARDABLE  0, 0, 306, 115
 STYLE DS_MODALFRAME | DS_3DLOOK | DS_CENTER | WS_POPUP | WS_CAPTION | 
@@ -475,6 +492,13 @@ BEGIN
      PUSHBUTTON      "Cancel",IDCANCEL,254,121,44,15
 END
 
+#undef	MISC_Y
+#undef	MISC_Y1
+#undef	MISC_Y2
+#define	MISC_Y	145
+#define	MISC_Y1	(MISC_Y+15)
+#define	MISC_Y2	(MISC_Y+37)
+
 DLG_OPTIONS_DRV DIALOG DISCARDABLE  0, 0, 287, 231
 STYLE DS_MODALFRAME | DS_CENTER | WS_POPUP | WS_CAPTION | WS_SYSMENU
 CAPTION "Advanced Options (DataSource)"
@@ -512,27 +536,36 @@ BEGIN
                     "Button",BS_AUTOCHECKBOX | WS_TABSTOP,105,125,105,10
     CONTROL         "Bools as Char",DRV_BOOLS_CHAR,"Button",BS_AUTOCHECKBOX | 
                     WS_TABSTOP,215,125,67,10
-    LTEXT           "&Cache Size:",IDC_STATIC,14,183,52,8
-    EDITTEXT        DRV_CACHE_SIZE,69,181,35,12,ES_AUTOHSCROLL
-    LTEXT           "Max &Varchar:",IDC_STATIC,13,161,54,8
-    EDITTEXT        DRV_VARCHAR_SIZE,70,160,35,12,ES_AUTOHSCROLL
-    LTEXT           "Max Lon&gVarChar:",IDC_STATIC,125,161,67,8
-    EDITTEXT        DRV_LONGVARCHAR_SIZE,199,160,35,12,ES_AUTOHSCROLL
-    LTEXT           "SysTable &Prefixes:",IDC_STATIC,126,178,61,18
-    EDITTEXT        DRV_EXTRASYSTABLEPREFIXES,199,181,71,12,ES_AUTOHSCROLL
+    GROUPBOX        "Miscellaneous",IDC_STATIC,5,MISC_Y,277,58
+    LTEXT           "Max &Varchar:",IDC_STATIC,13,MISC_Y1+1,54,8
+    EDITTEXT        DRV_VARCHAR_SIZE,70,MISC_Y1,35,12,ES_AUTOHSCROLL
+    LTEXT           "Max Lon&gVarChar:",IDC_STATIC,125,MISC_Y1+1,67,8
+    EDITTEXT        DRV_LONGVARCHAR_SIZE,199,MISC_Y1,35,12,ES_AUTOHSCROLL
+    LTEXT           "&Cache Size:",IDC_STATIC,14,MISC_Y2+1,52,8
+    EDITTEXT        DRV_CACHE_SIZE,69,MISC_Y2-1,35,12,ES_AUTOHSCROLL
+    LTEXT           "SysTable &Prefixes:",IDC_STATIC,126,MISC_Y2+1,61,18
+    EDITTEXT        DRV_EXTRASYSTABLEPREFIXES,199,MISC_Y2-1,71,12,ES_AUTOHSCROLL
     DEFPUSHBUTTON   "OK",IDOK,5,212,50,14,WS_GROUP
     PUSHBUTTON      "Cancel",IDCANCEL,81,211,50,15
     PUSHBUTTON      "Apply",IDAPPLY,156,212,50,14
     PUSHBUTTON      "Defaults",IDDEFAULTS,232,211,50,15
-    GROUPBOX        "Miscellaneous",IDC_STATIC,5,145,277,58
 END
 
-DLG_OPTIONS_DS DIALOG DISCARDABLE  0, 0, 306, 241
+#undef	CONNSETTINGS_Y
+#undef	KEEPALIVE_X
+#undef	KEEPALIVE_Y
+#undef	ENDLINE_Y
+#define	CONNSETTINGS_Y	185	// 189
+#define	KEEPALIVE_X	10
+#define	KEEPALIVE_Y	225
+#define	ENDLINE_Y	245	// 222
+
+DLG_OPTIONS_DS DIALOG DISCARDABLE  0, 0, 306, 260
 STYLE DS_MODALFRAME | DS_CENTER | WS_POPUP | WS_CAPTION | WS_SYSMENU
 CAPTION "Advanced Options (DataSource)"
 FONT 8, "MS Sans Serif"
 BEGIN
-    PUSHBUTTON      "Page 2",IDNEXTPAGE,49,5,40,15
+    PUSHBUTTON      "Page 3",IDNEXTPAGE,49,5,40,15
     PUSHBUTTON      "Page 1",IDPREVPAGE,5,5,40,15
     CONTROL         "&Read Only",DS_READONLY,"Button",BS_AUTOCHECKBOX | 
                     WS_GROUP | WS_TABSTOP,15,26,102,10
@@ -583,12 +616,18 @@ BEGIN
                     WS_GROUP | WS_TABSTOP,13,168,67,10
     CONTROL         "Fake &Index",DS_FAKEOIDINDEX,"Button",BS_AUTOCHECKBOX | 
                     WS_GROUP | WS_TABSTOP,80,168,59,10
-    LTEXT           "Connect &Settings:",IDC_STATIC,5,189,57,11
-    EDITTEXT        DS_CONNSETTINGS,90,189,211,27,ES_MULTILINE | 
+    LTEXT           "Connect &Settings:",IDC_STATIC,5,CONNSETTINGS_Y,57,11
+    EDITTEXT        DS_CONNSETTINGS,90,CONNSETTINGS_Y,211,27,ES_MULTILINE | 
                     ES_AUTOVSCROLL | ES_AUTOHSCROLL | ES_WANTRETURN
-    DEFPUSHBUTTON   "OK",IDOK,5,222,50,14,WS_GROUP
-    PUSHBUTTON      "Cancel",IDCANCEL,81,222,50,14
-    PUSHBUTTON      "Apply",IDAPPLY,156,222,50,14
+    GROUPBOX        "TCP KEEPALIVE setting (by sec)",IDC_STATIC,KEEPALIVE_X,KEEPALIVE_Y-10,210,25
+    CONTROL         "disable",DS_DISABLE_KEEPALIVE,"Button",BS_AUTOCHECKBOX | WS_GROUP, KEEPALIVE_X+5,KEEPALIVE_Y,50,10
+    LTEXT           "idle time",IDC_STATIC,KEEPALIVE_X+62,KEEPALIVE_Y,25,10,NOT WS_GROUP
+    EDITTEXT        DS_KEEPALIVETIME,KEEPALIVE_X+90,KEEPALIVE_Y,20,10, ES_AUTOHSCROLL | WS_TABSTOP
+    LTEXT           "interval",IDC_STATIC,KEEPALIVE_X+117,KEEPALIVE_Y,25,10,NOT WS_GROUP
+    EDITTEXT        DS_KEEPALIVEINTERVAL,KEEPALIVE_X+145,KEEPALIVE_Y,30,10, ES_AUTOHSCROLL | WS_TABSTOP
+    DEFPUSHBUTTON   "OK",IDOK,5,ENDLINE_Y,50,14,WS_GROUP
+    PUSHBUTTON      "Cancel",IDCANCEL,81,ENDLINE_Y,50,14
+    PUSHBUTTON      "Apply",IDAPPLY,156,ENDLINE_Y,50,14
     GROUPBOX        "Level of rollback on errors",IDC_STATIC,147,126,154,25
     CONTROL         "Nop",DS_NO_ROLLBACK,"Button",BS_AUTORADIOBUTTON | 
                     WS_GROUP,151,138,36,9
@@ -596,6 +635,40 @@ BEGIN
                     BS_AUTORADIOBUTTON | WS_TABSTOP,186,138,57,9
     CONTROL         "Statement",DS_STATEMENT_ROLLBACK,"Button",
                     BS_AUTORADIOBUTTON | WS_TABSTOP,247,138,47,9
+END
+
+#undef	KEEPALIVE_X
+#undef	KEEPALIVE_Y
+#define	KEEPALIVE_X	10
+#define	KEEPALIVE_Y	40
+
+DLG_OPTIONS_DS3 DIALOG DISCARDABLE  0, 0, 306, 243
+STYLE DS_MODALFRAME | DS_3DLOOK | DS_CENTER | WS_POPUP | WS_CAPTION | 
+    WS_SYSMENU
+CAPTION "Adavanced Options(Datasource 3)"
+FONT 8, "MS Sans Serif"
+BEGIN
+    PUSHBUTTON      "Page 3",IDNEXTPAGE,49,5,40,15
+    PUSHBUTTON      "Page 2",IDPREVPAGE,5,5,40,15
+
+    GROUPBOX        "TCP KEEPALIVE setting (by sec)",IDC_STATIC,KEEPALIVE_X,KEEPALIVE_Y-10,210,25
+    CONTROL         "disable",DS_DISABLE_KEEPALIVE,"Button",BS_AUTOCHECKBOX | WS_GROUP, KEEPALIVE_X+5,KEEPALIVE_Y,50,10
+    LTEXT           "idle time",IDC_STATIC,KEEPALIVE_X+62,KEEPALIVE_Y,25,10,NOT WS_GROUP
+    EDITTEXT        DS_KEEPALIVETIME,KEEPALIVE_X+90,KEEPALIVE_Y,20,10, ES_AUTOHSCROLL | WS_TABSTOP
+    LTEXT           "interval",IDC_STATIC,KEEPALIVE_X+117,KEEPALIVE_Y,25,10,NOT WS_GROUP
+    EDITTEXT        DS_KEEPALIVEINTERVAL,KEEPALIVE_X+145,KEEPALIVE_Y,30,10, ES_AUTOHSCROLL | WS_TABSTOP
+
+    GROUPBOX        "Distributed transaction log",IDC_STATIC,10,58,150,25
+    CONTROL         "default",DS_DEFAULT_DTCLOG,"Button",BS_AUTORADIOBUTTON | 
+                    WS_GROUP,15,70,40,10
+    CONTROL         "yes",DS_DTCLOG,"Button",
+                    BS_AUTORADIOBUTTON | WS_TABSTOP,55,70,30,10
+    CONTROL         "no",DS_NO_DTCLOG,"Button",
+                    BS_AUTORADIOBUTTON | WS_TABSTOP,85,70,50,10
+
+    DEFPUSHBUTTON   "OK",IDOK,5,224,50,14,WS_GROUP
+    PUSHBUTTON      "Cancel",IDCANCEL,66,224,50,14
+    PUSHBUTTON      "Apply",IDAPPLY,128,224,50,14
 END
 
 DLG_OPTIONS_GLOBAL DIALOG DISCARDABLE  0, 0, 306, 110

resource.h
@@ -16,6 +16,7 @@
 #define DLG_OPTIONS_DRV                 102
 #define DLG_OPTIONS_DS                  103
 #define DLG_OPTIONS_GLOBAL		104
+#define DLG_OPTIONS_DS3			105
 #define IDC_DSNAME			400
 #define IDC_DSNAMETEXT			401
 #define IDC_DESC			404
@@ -98,14 +99,20 @@
 #define IDC_TEST			1085
 #define DS_LOGDIR			1086
 #define DS_GSSAUTHUSEGSSAPI		1087
+#define DS_DISABLE_KEEPALIVE		1088
+#define DS_KEEPALIVETIME		1089
+#define DS_KEEPALIVEINTERVAL		1090
+#define DS_DEFAULT_DTCLOG		1091
+#define DS_DTCLOG			1092
+#define DS_NO_DTCLOG			1093
 
 // Next default values for new objects
 //
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_RESOURCE_VALUE        106
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1088
+#define _APS_NEXT_CONTROL_VALUE         1094
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif

commit 5a3183e864e2bd00cc19c5dcb96362ce7ad6b33b

Remove duplicated line in snprintf() which causes a crash introduced by the commit 1177281049e8bb3df5992da5dc1ca31ad892082e.

dlg_specific.c
@@ -427,7 +427,6 @@ inolog("hlen=%d", hlen);
 				ABBR_MAXLONGVARCHARSIZE "=%d;"
 				INI_INT8AS "=%d;"
 				ABBR_EXTRASYSTABLEPREFIXES "=%s;"
-				ABBR_EXTRASYSTABLEPREFIXES "=%s;"
 				"%s",
 				encoded_item,
 				ci->drivers.fetch_max,