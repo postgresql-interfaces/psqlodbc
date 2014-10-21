276cb5e413eafe970dbb60fcc41f56a7c1ddf2ac
Hiroshi Inoue <inoue@tpf.co.jp>
2014-10-21 16:57:28 +0900

    Allow building test suite in a different place other than the source code is. Change the test suite so that it works not only with unixODBC but also with iodbc.

---
+++ test/src/odbcini-gen.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+#
+#	This isn't a test application.
+#	Initial setting of odbc(inst).ini.
+#
+echo This isn\'t a test application
+echo Initializing odbc\(inst\).ini
+
+outini=odbc.ini
+outinstini=odbcinst.ini
+if test -s ${outini}; then exit; fi
+ 
+drvr=../.libs/psqlodbcw
+driver=${drvr}.so
+if test ! -e $driver ; then
+	driver=${drvr}.dll
+	if test ! -e $driver ; then
+		echo Failure:driver ${drvr}.so\(.dll\) not found
+		exit 2
+	fi
+fi
+
+echo creating $outinstini
+cat << _EOT_ > $outinstini
+[psqlodbc test driver]
+Description     = PostgreSQL ODBC driver (Unicode version), for regression tests
+Driver          = $driver
+Debug           = 0
+CommLog         = 1
+_EOT_
+
+server=localhost
+if test "${PGHOST}" != "" ; then
+	server=${PGHOST}
+fi
+port=5432
+if test "${PGPORT}" != "" ; then
+	port=${PGPORT}
+fi
+
+echo creating $outini
+cat << _EOT_ > $outini
+[psqlodbc_test_dsn]
+Description             = psqlodbc regression test DSN
+Driver          = psqlodbc test driver
+Trace           = No
+TraceFile               =
+Database                = contrib_regression
+Servername              = ${server}
+Username                =
+Password                =
+Port            = ${port}
+ReadOnly                = No
+RowVersioning           = No
+ShowSystemTables                = No
+ShowOidColumn           = No
+FakeOidIndex            = No
+ConnSettings            =
+_EOT_
