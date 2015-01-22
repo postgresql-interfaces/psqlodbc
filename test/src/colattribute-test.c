fa7c29f9424ddf12f72281ee1187e88f611dc8d7
Heikki Linnakangas <heikki.linnakangas@iki.fi>
2015-01-22 09:17:04 +0200

    Expand colattributes test case a bit.
    Test SQL_DESC_TYPE_NAME. There was an old report in the pgfoundry bug
    tracker that it didn't work correctly with interval types, (#1010745, from
    Farid Zidan on 2009-12-30), but it seems to work now.

--- test/src/colattribute-test.c
+++ test/src/colattribute-test.c
@@ -14,6 +14,7 @@ runtest(char *extra_conn_options)
 	int rc;
 	HSTMT hstmt = SQL_NULL_HSTMT;
 	SQLUSMALLINT i;
+	SQLSMALLINT numcols;
 
 	printf("Running tests with %s...\n", extra_conn_options);
 
@@ -32,21 +33,29 @@ runtest(char *extra_conn_options)
 	 */
 	printf("Testing SQLColAttribute...\n");
 	rc = SQLExecDirect(hstmt,
-			(SQLCHAR *) "SELECT '1'::int AS intcol, 'foobar'::text AS textcol, 'unknown string' AS unknowncol, 'varchar string' as varcharcol, '' as empty_varchar_col",
+			(SQLCHAR *) "SELECT '1'::int AS intcol, 'foobar'::text AS textcol, 'unknown string' AS unknowncol, 'varchar string'::varchar as varcharcol, ''::varchar as empty_varchar_col, 'varchar-5-col'::varchar(5) as varchar5col, '5 days'::interval day to second",
 			SQL_NTS);
 	CHECK_STMT_RESULT(rc, "SQLExecDirect failed", hstmt);
-	for (i = 1 ; i <= 5; i++)
+
+	rc = SQLNumResultCols(hstmt, &numcols);
+	CHECK_STMT_RESULT(rc, "SQLNumResultCols failed", hstmt);
+
+	for (i = 1 ; i <= numcols; i++)
 	{
 		char buffer[64];
 		SQLLEN number;
 
-		rc = SQLColAttribute(hstmt, i, SQL_DESC_LABEL, buffer, 64, NULL, NULL);
+		rc = SQLColAttribute(hstmt, i, SQL_DESC_LABEL, buffer, sizeof(buffer), NULL, NULL);
 		CHECK_STMT_RESULT(rc, "SQLColAttribute failed", hstmt);
 		printf("\n-- Column %d: %s --\n", i, buffer);
 
 		rc = SQLColAttribute(hstmt, i, SQL_DESC_OCTET_LENGTH, NULL, SQL_IS_INTEGER, NULL, &number);
 		CHECK_STMT_RESULT(rc, "SQLColAttribute failed", hstmt);
 		printf("SQL_DESC_OCTET_LENGTH: %d\n", (int) number);
+
+		rc = SQLColAttribute(hstmt, i, SQL_DESC_TYPE_NAME, buffer, sizeof(buffer), NULL, NULL);
+		CHECK_STMT_RESULT(rc, "SQLColAttribute failed", hstmt);
+		printf("SQL_DESC_TYPE_NAME: %s\n", buffer);
 	}
 
 	/* Clean up */
