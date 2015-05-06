355ac88016a53bf2aef79c84b69435ce52414a16
Heikki Linnakangas <heikki.linnakangas@iki.fi>
2015-05-07 02:19:45 +0300

    Don't define strlcat prototype if the system has strlcat()
    The #ifdef was wrong.
    
    Hopefully this fixes the build failure on OS X Maverick that Alex Dunn
    reported.

--- misc.h
+++ misc.h
@@ -19,9 +19,9 @@ extern "C" {
 #endif
 
 char	   *strncpy_null(char *dst, const char *src, ssize_t len);
-#ifndef	HAVE_STRLCPY
+#ifndef	HAVE_STRLCAT
 size_t		strlcat(char *, const char *, size_t);
-#endif /* HAVE_STRLCPY */
+#endif /* HAVE_STRLCAT */
 char	   *my_trim(char *string);
 char	   *make_string(const SQLCHAR *s, SQLINTEGER len, char *buf, size_t bufsize);
 SQLCHAR	*make_lstring_ifneeded(ConnectionClass *, const SQLCHAR *s, ssize_t len, BOOL);
