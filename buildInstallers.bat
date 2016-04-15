931b8e7d96e49d9f784d9aaa299f514597afe722
Hiroshi Inoue <h-inoue@dream.email.ne.jp>
2016-04-15 17:13:54 +0900

    Add a helper batch for installer/buildInstallers.ps1.

---
+++ buildInstallers.bat
@@ -0,0 +1,11 @@
+::
+::	Build installers of psqlodbc project
+::
+@echo off
+if "%1" == "/?" (
+	powershell Get-Help %~dp0\installer\buildInstallers.ps1 -detailed
+) else if "%1" == "-?" (
+	powershell Get-Help %~dp0\installer\buildInstallers.ps1 %2 %3 %4 %5 %6 %7 %8 %9
+) else (
+	powershell %~dp0\installer\buildInstallers.ps1 %*
+)
