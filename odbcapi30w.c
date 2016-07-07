postgresql-interfaces/psqlodbc

commit 4d44165af04d40e8aad405a9fbe31d7304c654c8

Fix NULL dereferences which causes application crash. Patch by Tsunakawa, Takayuki.

odbcapi30w.c
@@ -158,7 +158,7 @@ SQLGetDescFieldW(SQLHDESC hdesc, SQLSMALLINT iRecord, SQLSMALLINT iField,
 	CSTR func = "SQLGetDescFieldW";
 	RETCODE	ret;
 	SQLINTEGER		blen = 0, bMax,	*pcbV;
-        char    *rgbV = NULL;
+        char    *rgbV = NULL, *rgbVt;
 
 	mylog("[%s]", func);
 	switch (iField)
@@ -175,10 +175,16 @@ SQLGetDescFieldW(SQLHDESC hdesc, SQLSMALLINT iRecord, SQLSMALLINT iField,
 		case SQL_DESC_TABLE_NAME:
 		case SQL_DESC_TYPE_NAME:
 			bMax = cbValueMax * 3 / WCLEN;
-			rgbV = malloc(bMax + 1);
+			rgbVt = malloc(bMax + 1);
 			pcbV = &blen;
-			for (;; bMax = blen + 1, rgbV = realloc(rgbV, bMax))
+			for (;; bMax = blen + 1, rgbVt = realloc(rgbV, bMax))
 			{
+				if (!rgbVt)
+				{
+					ret = SQL_ERROR;
+					break;
+				}
+				rgbV = rgbVt;
 				ret = PGAPI_GetDescField(hdesc, iRecord, iField, rgbV, bMax, pcbV);
 				if (SQL_SUCCESS_WITH_INFO != ret || blen < bMax)
 					break;
@@ -279,7 +285,7 @@ SQLColAttributeW(SQLHSTMT	hstmt,
 	RETCODE	ret;
 	StatementClass	*stmt = (StatementClass *) hstmt;
 	SQLSMALLINT	*rgbL, blen = 0, bMax;
-        char    *rgbD = NULL;
+        char    *rgbD = NULL, *rgbDt;
 
 	mylog("[%s]", func);
 	ENTER_STMT_CS(stmt);
@@ -300,10 +306,16 @@ SQLColAttributeW(SQLHSTMT	hstmt,
 		case SQL_DESC_TYPE_NAME:
 		case SQL_COLUMN_NAME:
 			bMax = cbCharAttrMax * 3 / WCLEN;
-			rgbD = malloc(bMax);
+			rgbDt = malloc(bMax);
 			rgbL = &blen;
-			for (;; bMax = blen + 1, rgbD = realloc(rgbD, bMax))
+			for (;; bMax = blen + 1, rgbDt = realloc(rgbD, bMax))
 			{
+				if (!rgbDt)
+				{
+					ret = SQL_ERROR;
+					break;
+				}
+				rgbD = rgbDt;
 				ret = PGAPI_ColAttributes(hstmt, iCol, iField, rgbD,
 					bMax, rgbL, pNumAttr);
 				if (SQL_SUCCESS_WITH_INFO != ret || blen < bMax)
@@ -349,7 +361,7 @@ SQLGetDiagFieldW(SQLSMALLINT	fHandleType,
 	CSTR		func = "SQLGetDiagFieldW";
 	RETCODE		ret;
 	SQLSMALLINT	*rgbL, blen = 0, bMax;
-	char	   *rgbD = NULL;
+	char	   *rgbD = NULL, *rgbDt;
 
 	mylog("[[%s]] Handle=(%u,%p) Rec=%d Id=%d info=(%p,%d)\n", func, fHandleType,
 			handle, iRecord, fDiagField, rgbDiagInfo, cbDiagInfoMax);
@@ -363,11 +375,14 @@ SQLGetDiagFieldW(SQLSMALLINT	fHandleType,
 		case SQL_DIAG_SQLSTATE:
 		case SQL_DIAG_SUBCLASS_ORIGIN:
 			bMax = cbDiagInfoMax * 3 / WCLEN + 1;
-			if (rgbD = malloc(bMax), !rgbD)
+			if (rgbDt = malloc(bMax), !rgbD)
 				return SQL_ERROR;
 			rgbL = &blen;
-			for (;; bMax = blen + 1, rgbD = realloc(rgbD, bMax))
+			for (;; bMax = blen + 1, rgbDt = realloc(rgbD, bMax))
 			{
+				if (!rgbDt)
+					return SQL_ERROR;
+				rgbD = rgbDt;
 				ret = PGAPI_GetDiagField(fHandleType, handle, iRecord, fDiagField, rgbD,
 					bMax, rgbL);
 				if (SQL_SUCCESS_WITH_INFO != ret || blen < bMax)

connection.c
@@ -50,7 +50,6 @@
 								 * at a time */
 
 static void CC_lookup_lo(ConnectionClass *self);
-static char *CC_create_errormsg(ConnectionClass *self);
 static int  CC_close_eof_cursors(ConnectionClass *self);
 
 static void LIBPQ_update_transaction_status(ConnectionClass *self);
@@ -522,7 +521,6 @@ CC_clear_error(ConnectionClass *self)
 		self->__error_message = NULL;
 	}
 	self->sqlstate[0] = '\0';
-	self->errormsg_created = FALSE;
 	CONNLOCK_RELEASE(self);
 }
 
@@ -1218,30 +1216,6 @@ mylog("max_identifier_length=%d\n", len);
 	return len < 0 ? 0 : len;
 }
 
-/*
- *	Create a more informative error message by concatenating the connection
- *	error message with its socket error message.
- *
- * XXX: actually, there is no such thing as socket error message anymore
- */
-static char *
-CC_create_errormsg(ConnectionClass *self)
-{
-	char	msg[4096];
-
-	mylog("enter CC_create_errormsg\n");
-
-	msg[0] = '\0';
-
-	if (CC_get_errormsg(self))
-		strncpy_null(msg, CC_get_errormsg(self), sizeof(msg));
-
-	mylog("msg = '%s'\n", msg);
-
-	mylog("exit CC_create_errormsg\n");
-	return strdup(msg);
-}
-
 
 void
 CC_set_error(ConnectionClass *self, int number, const char *message, const char *func)
@@ -1274,20 +1248,10 @@ char
 CC_get_error(ConnectionClass *self, int *number, char **message)
 {
 	int			rv;
-	char *msgcrt;
 
 	mylog("enter CC_get_error\n");
 
 	CONNLOCK_ACQUIRE(self);
-	/* Create a very informative errormsg if it hasn't been done yet. */
-	if (!self->errormsg_created)
-	{
-		msgcrt = CC_create_errormsg(self);
-		if (self->__error_message)
-			free(self->__error_message);
-		self->__error_message = msgcrt;
-		self->errormsg_created = TRUE;
-	}
 
 	if (CC_get_errornumber(self))
 	{
@@ -1755,6 +1719,13 @@ CC_send_query_append(ConnectionClass *self, const char *query, QueryInfo *qi, UD
 				if (query_completed)	/* allow for "show" style notices */
 				{
 					res->next = QR_Constructor();
+					if (!res->next)
+					{
+						CC_set_error(self, CONNECTION_COULD_NOT_RECEIVE, "Could not create result info in send_query.", func);
+						ReadyToReturn = TRUE;
+						retres = NULL;
+						break;
+					}
 					res = res->next;
 					nrarg.res = res;
 				}
@@ -1904,6 +1875,13 @@ inolog("Discarded the first SAVEPOINT\n");
 				if (query_completed)
 				{
 					res->next = QR_Constructor();
+					if (!res->next)
+					{
+						CC_set_error(self, CONNECTION_COULD_NOT_RECEIVE, "Could not create result info in send_query.", func);
+						ReadyToReturn = TRUE;
+						retres = NULL;
+						break;
+					}
 					res = res->next;
 					nrarg.res = res;
 				}
@@ -2369,7 +2347,8 @@ CC_get_current_schema(ConnectionClass *conn)
 				if (curschema)
 					conn->current_schema = strdup(curschema);
 			}
-			conn->current_schema_valid = TRUE;
+			if (conn->current_schema)
+				conn->current_schema_valid = TRUE;
 		}
 		QR_Destructor(res);
 	}
@@ -3050,6 +3029,7 @@ PgDtc_isolate(void *self, DWORD option)
 		return newconn;
 	}
 	newconn = CC_Constructor();
+	if (!newconn) return NULL;
 	CC_copy_conninfo(&newconn->connInfo, &sconn->connInfo);
 	CC_initialize_pg_version(newconn);
 	newconn->asdum = sconn->asdum;

connection.h
@@ -354,8 +354,6 @@ struct ConnectionClass_
 	DriverToDataSourceProc DriverToDataSource;
 	char		transact_status;	/* Is a transaction is currently
 						 * in progress */
-	char		errormsg_created;	/* has an informative error msg
-						 * been created ? */
 	char		pg_version[MAX_INFO_STRING];	/* Version of PostgreSQL
 							 * we're connected to -
 							 * DJP 25-1-2001 */

convert.c
@@ -3046,6 +3046,11 @@ inolog("type=%d concur=%d\n", stmt->options.cursor_type, stmt->options.scroll_co
 		}
 		npos -= qp->declare_pos;
 		stmt->load_statement = malloc(npos + 1);
+		if (!stmt->load_statement)
+		{
+			retval = SQL_ERROR;
+			goto cleanup;
+		}
 		memcpy(stmt->load_statement, qb->query_statement + qp->declare_pos, npos);
 		stmt->load_statement[npos] = '\0';
 	}
@@ -4160,10 +4165,13 @@ inolog("ipara=%p paramType=%d %d proc_return=%d\n", ipara, ipara ? ipara->paramT
 			if (SQL_NTS == used)
 				used = strlen(buffer);
 			allocbuf = malloc(WCLEN * (used + 1));
-			used = msgtowstr(buffer, (int) used, (LPWSTR) allocbuf, (int) (used + 1));
-			buf = ucs2_to_utf8((SQLWCHAR *) allocbuf, used, &used, FALSE);
-			free(allocbuf);
-			allocbuf = buf;
+			if (allocbuf)
+			{
+				used = msgtowstr(buffer, (int) used, (LPWSTR) allocbuf, (int) (used + 1));
+				buf = ucs2_to_utf8((SQLWCHAR *) allocbuf, used, &used, FALSE);
+				free(allocbuf);
+				allocbuf = buf;
+			}
 #else
 			buf = buffer;
 #endif /* WIN_UNICODE_SUPPORT */

descriptor.c
@@ -352,7 +352,8 @@ static void ARDFields_copy(const ARDFields *src, ARDFields *target)
 	if (src->bookmark)
 	{
 		BindInfoClass *bookmark = ARD_AllocBookmark(target);
-		BindInfoClass_copy(src->bookmark, bookmark);
+		if (bookmark)
+			BindInfoClass_copy(src->bookmark, bookmark);
 	}
 	if (src->allocated <= 0)
 	{
@@ -364,6 +365,8 @@ static void ARDFields_copy(const ARDFields *src, ARDFields *target)
 		int	i;
 
 		target->bindings = malloc(target->allocated * sizeof(BindInfoClass));
+		if (!target->bindings)
+			target->allocated = 0;
 		for (i = 0; i < target->allocated; i++)
 			BindInfoClass_copy(&src->bindings[i], &target->bindings[i]);
 	}
@@ -379,7 +382,8 @@ static void APDFields_copy(const APDFields *src, APDFields *target)
 	if (src->bookmark)
 	{
 		target->bookmark = malloc(sizeof(ParameterInfoClass));
-		ParameterInfoClass_copy(src->bookmark, target->bookmark);
+		if (target->bookmark)
+			ParameterInfoClass_copy(src->bookmark, target->bookmark);
 	}
 	if (src->allocated <= 0)
 	{
@@ -391,6 +395,8 @@ static void APDFields_copy(const APDFields *src, APDFields *target)
 		int	i;
 
 		target->parameters = malloc(target->allocated * sizeof(ParameterInfoClass));
+		if (!target->parameters)
+			target->allocated = 0;
 		for (i = 0; i < target->allocated; i++)
 			ParameterInfoClass_copy(&src->parameters[i], &target->parameters[i]);
 	}
@@ -413,6 +419,8 @@ static void IPDFields_copy(const IPDFields *src, IPDFields *target)
 		int	i;
 
 		target->parameters = (ParameterImplClass *) malloc(target->allocated * sizeof(ParameterImplClass));
+		if (!target->parameters)
+			target->allocated = 0;
 		for (i = 0; i < target->allocated; i++)
 			ParameterImplClass_copy(&src->parameters[i], &target->parameters[i]);
 	}

dlg_specific.c
@@ -1544,6 +1544,8 @@ decode(const char *in)
 		return out;
 	}
 	outs = (char *) malloc(ilen + 1);
+	if (!outs)
+		return out;
 	for (i = 0; i < ilen; i++)
 	{
 		inc = in[i];
@@ -1687,6 +1689,8 @@ char *extract_extra_attribute_setting(const pgNAME setting, const char *attr)
 	if (!sptr)
 		return NULL;
 	rptr = malloc(len + 1);
+	if (!rptr)
+		return NULL;
 	memcpy(rptr, sptr, len);
 	rptr[len] = '\0';
 	mylog("extracted a %s '%s' from %s\n", attr, rptr, str);

drvconn.c
@@ -47,6 +47,7 @@ static char * hide_password(const char *str)
 
 	if (!str)	return NULL;
 	outstr = strdup(str);
+	if (!outstr) return NULL;
 	if (pwdp = strstr(outstr, "PWD="), !pwdp)
 		pwdp = strstr(outstr, "pwd=");
 	if (pwdp)

environ.c
@@ -174,7 +174,8 @@ ER_Dup(const PG_ErrorInfo *self)
 	if (self->errorsize  > 0)
 		alsize += self->errorsize;
 	new = (PG_ErrorInfo *) malloc(alsize);
-	memcpy(new, self, alsize);
+	if (new)
+		memcpy(new, self, alsize);
 
 	return new;
 }

commit a64e4f555eb5dbcabd9976617cbe6568ef745fd4

Fix the bug introduced by the commit 4d44165af04d40e8aad405a9fbe31d7304c654c8 that SQLGetDiagFieldW, SQLGetDescFieldW or SQLColAttributeW mistakenly returns SQL_ERROR.

odbcapi30w.c
@@ -175,9 +175,9 @@ SQLGetDescFieldW(SQLHDESC hdesc, SQLSMALLINT iRecord, SQLSMALLINT iField,
 		case SQL_DESC_TABLE_NAME:
 		case SQL_DESC_TYPE_NAME:
 			bMax = cbValueMax * 3 / WCLEN;
-			rgbVt = malloc(bMax + 1);
+			rgbV = malloc(bMax + 1);
 			pcbV = &blen;
-			for (;; bMax = blen + 1, rgbVt = realloc(rgbV, bMax))
+			for (rgbVt = rgbV;; bMax = blen + 1, rgbVt = realloc(rgbV, bMax))
 			{
 				if (!rgbVt)
 				{
@@ -306,9 +306,9 @@ SQLColAttributeW(SQLHSTMT	hstmt,
 		case SQL_DESC_TYPE_NAME:
 		case SQL_COLUMN_NAME:
 			bMax = cbCharAttrMax * 3 / WCLEN;
-			rgbDt = malloc(bMax);
+			rgbD = malloc(bMax);
 			rgbL = &blen;
-			for (;; bMax = blen + 1, rgbDt = realloc(rgbD, bMax))
+			for (rgbDt = rgbD;; bMax = blen + 1, rgbDt = realloc(rgbD, bMax))
 			{
 				if (!rgbDt)
 				{
@@ -375,13 +375,16 @@ SQLGetDiagFieldW(SQLSMALLINT	fHandleType,
 		case SQL_DIAG_SQLSTATE:
 		case SQL_DIAG_SUBCLASS_ORIGIN:
 			bMax = cbDiagInfoMax * 3 / WCLEN + 1;
-			if (rgbDt = malloc(bMax), !rgbD)
+			if (rgbD = malloc(bMax), !rgbD)
 				return SQL_ERROR;
 			rgbL = &blen;
-			for (;; bMax = blen + 1, rgbDt = realloc(rgbD, bMax))
+			for (rgbDt = rgbD;; bMax = blen + 1, rgbDt = realloc(rgbD, bMax))
 			{
 				if (!rgbDt)
+				{
+					free(rgbD);
 					return SQL_ERROR;
+				}
 				rgbD = rgbDt;
 				ret = PGAPI_GetDiagField(fHandleType, handle, iRecord, fDiagField, rgbD,
 					bMax, rgbL);