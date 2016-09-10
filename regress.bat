17dad8721506f864ae91ffc9ec0fc7309ec26771
Hiroshi Inoue <h-inoue@dream.email.ne.jp>
2016-09-10 15:07:59 +0900

    Modify helper batch commands so that they can handle path names which contain spaces.

--- regress.bat
+++ regress.bat
@@ -3,9 +3,9 @@
 ::
 @echo off
 if "%1" == "/?" (
-	powershell Get-Help %~dp0\winbuild\regress.ps1 -detailed
+	powershell Get-Help '%~dp0\winbuild\regress.ps1' -detailed
 ) else if "%1" == "-?" (
-	powershell Get-Help %~dp0\winbuild\regress.ps1 %2 %3 %4 %5 %6 %7 %8 %9
+	powershell Get-Help '%~dp0\winbuild\regress.ps1' %2 %3 %4 %5 %6 %7 %8 %9
 ) else (
-	powershell %~dp0\winbuild\regress.ps1 %*
+	powershell "& '%~dp0\winbuild\regress.ps1' %*"
 )
