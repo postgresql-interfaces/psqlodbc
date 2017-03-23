d4538ef614e4d1ecc47c4dfa75b87b1758713ec9
Hiroshi Inoue <h-inoue@dream.email.ne.jp>
2017-03-23 17:59:42 +0900

    Modify odbcini-gen.sh so that it generates ANSI driver and dsn as well.

--- test/odbcini-gen.sh
+++ test/odbcini-gen.sh
@@ -16,20 +16,65 @@ if test ! -e $driver ; then
 	fi
 fi
 
+drvra=../.libs/psqlodbca
+drivera=${drvra}.so
+if test ! -e $drivera ; then
+	drivera=${drvra}.dll
+	if test ! -e $drivera ; then
+		echo Failure:driver ${drvra}.so\(.dll\) not found
+		exit 2
+	fi
+fi
+
 echo creating $outinstini
 cat << _EOT_ > $outinstini
-[psqlodbc test driver]
+[ODBC]
+Trace = off
+TraceFile =
+[PostgreSQL Unicode]
 Description     = PostgreSQL ODBC driver (Unicode version), for regression tests
 Driver          = $driver
 Debug           = 0
-CommLog         = 1
+CommLog         = 0
+[PostgreSQL ANSI]
+Description     = PostgreSQL ODBC driver (ANSI version), for regression tests
+Driver          = $drivera
+Debug           = 0
+CommLog         = 0
 _EOT_
 
 echo creating $outini: $@
+# Unicode
 cat << _EOT_ > $outini
 [psqlodbc_test_dsn]
 Description             = psqlodbc regression test DSN
-Driver          = psqlodbc test driver
+Driver          = PostgreSQL Unicode
+Trace           = No
+TraceFile               =
+Database                = contrib_regression
+Servername              =
+Username                =
+Password                =
+Port                    =
+ReadOnly                = No
+RowVersioning           = No
+ShowSystemTables                = No
+ShowOidColumn           = No
+FakeOidIndex            = No
+ConnSettings            =
+_EOT_
+
+# Add any extra options from the command line
+for opt in "$@"
+do
+    echo "${opt}" >> $outini
+done
+
+# ANSI
+cat << _EOT_ >> $outini
+[psqlodbc_test_dsna]
+Description             = psqlodbc ansi regression test DSN
+Driver          = PostgreSQL ANSI
 Trace           = No
 TraceFile               =
 Database                = contrib_regression
