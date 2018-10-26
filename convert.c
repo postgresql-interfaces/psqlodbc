postgresql-interfaces/psqlodbc

commit 499fb29b80cfaf60fffd81549b36ec2def1cf21d

Remove obsolete maps pointed out by Daniel Cory <dcory@tableau.com>. 1. POWER -> pow 2. CONCAT -> textcat 3. LEFT -> ltrunc 4. RIGHT -> rtrunc

convert.c
@@ -79,16 +79,16 @@ static const struct
 } mapFuncs[] = {
 /*	{ "ASCII",		 "ascii"	  }, built_in */
 	{"CHAR", "chr($*)" },
-	{"CONCAT", "textcat($*)" },
+/*	{"CONCAT",		 "concat"	  }, built_in */
 /*	{ "DIFFERENCE", "difference" }, how to ? */
 	{"INSERT", "substring($1 from 1 for $2 - 1) || $4 || substring($1 from $2 + $3)" },
 	{"LCASE", "lower($*)" },
-	{"LEFT", "ltrunc($*)" },
+/*	{"LEFT",		 "left"		  }, built_in */
 	{"%2LOCATE", "strpos($2,  $1)" },	/* 2 parameters */
 	{"%3LOCATE", "strpos(substring($2 from $3), $1) + $3 - 1" },	/* 3 parameters */
 	{"LENGTH", "char_length($*)"},
 /*	{ "LTRIM",		 "ltrim"	  }, built_in */
-	{"RIGHT", "rtrunc($*)" },
+/*	{"RIGHT",		 "right"	  }, built_in */
 	{"SPACE", "repeat(' ', $1)" },
 /*	{ "REPEAT",		 "repeat"	  }, built_in */
 /*	{ "REPLACE", "replace" }, ??? */
@@ -112,7 +112,7 @@ static const struct
 	{"LOG10", "log($*)" },
 /*	{ "MOD",		 "mod" 		  }, built_in */
 /*	{ "PI",			 "pi" 		  }, built_in */
-	{"POWER", "pow($*)" },
+/*	{"POWER", 		 "power"	  }, built_in */
 /*	{ "RADIANS",		 "radians"	  }, built_in */
 	{"%0RAND", "random()" },	/* 0 parameters */
 	{"%1RAND", "(setseed($1) * .0 + random())" },	/* 1 parameters */

commit 61b67b9b98c47ce5997af2ec124cdcada040fc88

Fix the regression test failure in odbc-escapes caused by the commit 499fb29b80cfaf60fffd81549b36ec2def1cf21d. The parameters should be cast because parameters of concat() function are variadic "any".

convert.c
@@ -79,7 +79,7 @@ static const struct
 } mapFuncs[] = {
 /*	{ "ASCII",		 "ascii"	  }, built_in */
 	{"CHAR", "chr($*)" },
-/*	{"CONCAT",		 "concat"	  }, built_in */
+	{"CONCAT", "concat($1::text, $2::text)" },
 /*	{ "DIFFERENCE", "difference" }, how to ? */
 	{"INSERT", "substring($1 from 1 for $2 - 1) || $4 || substring($1 from $2 + $3)" },
 	{"LCASE", "lower($*)" },