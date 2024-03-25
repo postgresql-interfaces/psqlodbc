de40b7ae81e65049421e0dd497460b1e9570ce0f
Dave Cramer <davecramer@gmail.com>
2024-03-25 17:49:03 -0400

    use powershell

--- .github/workflows/setenvx86.ps1
+++
@@ -1,8 +0,0 @@
-pushd "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\"
-cmd /c "vcvars32.bat&set" |
- foreach {
-   if ($_ -match "=") {
-     $v = $_.split("="); set-item -force -path "ENV:\$($v[0])"  -value "$($v[1])"
-   }
- }
- popd
