11873ade60248604e5865b23118dfbdba31143e5
Dave Cramer <davecramer@gmail.com>
2025-01-23 11:10:35 -0500

    test with just unique keys

--- test/sampletables.sql
+++ test/sampletables.sql
@@ -32,7 +32,8 @@ INSERT INTO booltab VALUES (3, 'true', true);
 INSERT INTO booltab VALUES (4, 'false', false);
 INSERT INTO booltab VALUES (5, 'not', false);
 
-CREATE TABLE test_special( id integer primary key, ival integer);
+CREATE TABLE test_special( id integer, ival integer);
+CREATE UNIQUE index test_special_id on public.test_special(id);
 CREATE UNIQUE index test_special_ui on public.test_special(ival);
 
 -- View
